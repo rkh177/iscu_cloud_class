64a5d351fe19846f579974bac0f660f159d229e8
Kihyun Ryu <ryukh@Kihyunui-MacBookAir.local>
2026-09-06 18:29:06 +0900

    change message

--- week01/hello-cloud-starter/app/page.js
+++ week01/hello-cloud-starter/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hello Cloud!</h1>
-      <p>Welcome to my cloud computing class!</p>
+      <p>Welcome to my cloud computing class! Auto-updated on Vercel.</p>
     </main>
   );
 }
